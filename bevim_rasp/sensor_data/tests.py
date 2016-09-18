1f250db6b38c57e712aec3612e78f1d591b7dcef
Italo Paiva <italosensei@hotmail.com>
2016-09-18 11:18:59 -0300

    Adding tests to the rest API

--- bevim_rasp/sensor_data/tests.py
+++ bevim_rasp/sensor_data/tests.py
@@ -1,3 +1,55 @@
-from django.test import TestCase
+from django.core.urlresolvers import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from .models import Sensor, Acceleration, Speed, Amplitude, Frequency
+from .serializers import DataSerializer
 
-# Create your tests here.
+
+class DataRestTest(APITestCase):
+
+    def setUp(self):
+        self.sensor1 = Sensor.objects.create(name="S1")
+        self.acceleration1 = Acceleration.objects.create(sensor=self.sensor1, value=234);
+        self.acceleration2 = Acceleration.objects.create(sensor=self.sensor1, value=987);
+        self.speed = Speed.objects.create(sensor=self.sensor1, value=432);
+        self.amplitude = Amplitude.objects.create(sensor=self.sensor1, value=123);
+        self.frequency = Frequency.objects.create(sensor=self.sensor1, value=20);
+
+    def test_get_sensor(self):
+        url = reverse('sensor_rest')
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        expected_response = [{"id": self.sensor1.pk, "name": self.sensor1.name}]
+        self.assertEqual(response.data, expected_response)
+
+    def test_get_acceleration(self):
+        url = reverse('acceleration_rest')
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        accelerations = Acceleration.objects.all()
+        expected_response = DataSerializer(accelerations, many=True).data
+        self.assertEqual(response.data, expected_response)
+
+    def test_get_speed(self):
+        url = reverse('speed_rest')
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        speeds = Speed.objects.all()
+        expected_response = DataSerializer(speeds, many=True).data
+        self.assertEqual(response.data, expected_response)
+
+    def test_get_amplitude(self):
+        url = reverse('amplitude_rest')
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        amplitudes = Amplitude.objects.all()
+        expected_response = DataSerializer(amplitudes, many=True).data
+        self.assertEqual(response.data, expected_response)
+
+    def test_get_frequency(self):
+        url = reverse('frequency_rest')
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        frequencies = Frequency.objects.all()
+        expected_response = DataSerializer(frequencies, many=True).data
+        self.assertEqual(response.data, expected_response)
