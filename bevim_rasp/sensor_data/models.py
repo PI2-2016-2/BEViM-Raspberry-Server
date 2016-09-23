1b2f8dcbe205839cfc8a614d9f9622bc8e7473df
Italo Paiva <italosensei@hotmail.com>
2016-09-23 17:18:16 -0300

    Adding axis attributes on sensor data model

--- bevim_rasp/sensor_data/models.py
+++ bevim_rasp/sensor_data/models.py
@@ -21,7 +21,9 @@ class Sensor(models.Model):
 
 class Data(models.Model):
     sensor = models.ForeignKey(Sensor, verbose_name='Sensor')
-    value = models.IntegerField(default=0)
+    x_value = models.DecimalField(max_digits=4, decimal_places=2)
+    y_value = models.DecimalField(max_digits=4, decimal_places=2)
+    z_value = models.DecimalField(max_digits=4, decimal_places=2)
     timestamp = models.DateTimeField(auto_now_add=True)
 
 
